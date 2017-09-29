f30a4332973517e57bab2711fffa88c0855aeea6
p2e2t7@gmail.com <Petch0869898562>
2017-09-29 12:30:12 +0700

    เปลี่ยน Layout

--- MyAppplications/MyAppplicationsPage.xaml.cs
+++ MyAppplications/MyAppplicationsPage.xaml.cs
@@ -7,6 +7,15 @@ namespace MyAppplications
         public MyAppplicationsPage()
         {
             InitializeComponent();
+            genderPicker.Items.Add("ชาย");
+            genderPicker.Items.Add("หญิง");
+
+            salary.ValueChanged += Salary_ValueChanged;
+        }
+
+        void Salary_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
         }
     }
 }
